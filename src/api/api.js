63a2c8486f3022d0b8eab652f345d46d682bfaa7
uhomuho <45100335+uhomuho@users.noreply.github.com>
2020-07-20 15:00:31 +0500

    Update

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,15 @@
 import axios from 'axios'
 
+axios.defaults.headers.common['Content-Type'] = 'multipart/form-data'
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
+
 export default () => {
+	let url = "//serene-savannah-08565.herokuapp.com"
+	if (process.env.NODE_ENV == 'development') {
+		url = "http://localhost:8081"
+	}
+
 	return axios.create({
-		baseURL: 'https://serene-savannah-08565.herokuapp.com',
-		headers: {
-			'Content-Type': 'multipart/form-data',
-			'Access-Control-Allow-Origin': '*'
-		}
+		baseURL: url
 	})
 }
